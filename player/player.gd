26652b5b9ed24b4b12672759d61a1fc74ede5f90
luizfelipe <luizfelipefalcao40@gmail.com>
2026-05-04 15:24:36 -0300

    feat: pickup, door and footstep sounds

--- player/player.gd
+++ player/player.gd
@@ -7,6 +7,7 @@ extends CharacterBody2D
 @export var recharge_bar: ProgressBar
 @export var recharge_audio: AudioStreamPlayer
 @export var battery_text_animation : AnimationPlayer
+@export var sound_player: AnimationPlayer
 @export var recharge_duration := 2.0
 @export var death_fade_duration := 0.3
 
@@ -43,9 +44,12 @@ func _physics_process(_delta):
 	if direction.length_squared() == 0:
 		_silhouette_sprite.play("idle")
 		_sprite.play("idle")
+		sound_player.pause()
+
 	else:
 		_silhouette_sprite.play("walk")
 		_sprite.play("walk")
+		sound_player.play("footstep_loop")
 
 	move_and_slide()
 
@@ -66,6 +70,7 @@ func _process(_delta):
 				_silhouette_sprite.play("idle")
 				_silhouette_sprite.frame = 0
 				_silhouette_sprite.pause()
+				sound_player.pause()
 		
 			_can_move = false
 			_can_interact = false
@@ -99,6 +104,7 @@ func _on_timeline_started():
 	_can_interact = false
 	_silhouette_sprite.play("idle")
 	_sprite.play("idle")
+	sound_player.pause()
 	set_process(false)
 
 
@@ -114,5 +120,6 @@ func _on_game_over():
 	set_physics_process(false)
 	_sprite.play("idle")
 	_silhouette_sprite.play("idle")
+	sound_player.pause()
 	var tween = create_tween()
 	tween.tween_property(_sprite, "modulate", Color.TRANSPARENT, death_fade_duration)
